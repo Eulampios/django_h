63f69d6f87bb77b61c7d6d54c14d36b5395afbc9
mutovkin_av <jw.buzd@gmail.com>
2022-05-18 17:48:44 +0300

    email to link v.1

--- mainapp/views.py
+++ mainapp/views.py
@@ -6,6 +6,19 @@ from datetime import datetime
 class ContactsView(TemplateView):
     template_name = 'mainapp/contacts.html'
 
+    def get_context_data(self, **kwargs):
+        context_data = super().get_context_data(**kwargs)
+        context_data['contacts'] = [
+            {
+                'map': 'https://yandex.ru/map-widget/v1/-/CCUAZHcrhA',
+                'city': 'Санкт‑Петербург',
+                'phone': '+7-999-11-11111',
+                'email': 'geeklab@spb.ru',
+                'adress': 'территория Петропавловская крепость, 3Ж'
+            }
+        ]
+        return context_data
+
 
 class CoursesListView(TemplateView):
     template_name = 'mainapp/courses_list.html'
@@ -33,19 +46,19 @@ class NewsView(TemplateView):
                 'title': 'Заголовок новости 1',
                 'description': 'Описание новости 1',
                 'date': datetime.now()
-            },  {
+            }, {
                 'title': 'Заголовок новости 2',
                 'description': 'Описание новости 2',
                 'date': datetime.now()
-            },  {
+            }, {
                 'title': 'Заголовок новости 3',
                 'description': 'Описание новости 3',
                 'date': datetime.now()
-            },  {
+            }, {
                 'title': 'Заголовок новости 4',
                 'description': 'Описание новости 4',
                 'date': datetime.now()
-            },  {
+            }, {
                 'title': 'Заголовок новости 5',
                 'description': 'Описание новости 5',
                 'date': datetime.now()
