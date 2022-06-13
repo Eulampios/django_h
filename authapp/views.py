74f3e421506c25582c74a4e2cf7e7a573faacc9f
Eulampios <88624719+Eulampios@users.noreply.github.com>
2022-06-13 20:30:05 +0300

    init commit

--- authapp/views.py
+++ authapp/views.py
@@ -1,5 +1,10 @@
+from django.contrib import messages
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
+from django.urls import reverse
 from django.views.generic import TemplateView
+
+from authapp.models import User
 
 
 class LoginView(TemplateView):
@@ -14,6 +19,35 @@ class RegisterView(TemplateView):
     extra_context = {
         'title' : 'Регистрация пользователя'
     }
+
+    def post(self, request, *args, **kwargs):
+        try:
+            if all(
+                    (
+                        request.POST.get('username'),
+                        request.POST.get('email'),
+                        request.POST.get('password1'),
+                        request.POST.get('password2'),
+                        request.POST.get('first_name'),
+                        request.POST.get('last_name'),
+                        request.POST.get('password1') == request.POST.get('password2'),
+                    )
+            ):
+                new_user = User.objects.create(
+                    username=request.POST.get('username'),
+                    first_name=request.POST.get('first_name'),
+                    last_name=request.POST.get('last_name'),
+                    email=request.POST.get('email'),
+                    age=request.POST.get('age') if request.POST.get('age') else 0,
+                    avatar=request.FILES.get('avatar')
+                )
+                new_user.set_password(request.POST.get('password1'))
+                new_user.save()
+                messages.add_message(request, messages.INFO, 'Регистрация прошла успешно')
+                return HttpResponseRedirect(reverse('authapp:login'))
+        except:
+            messages.add_message(request, messages.WARNING, 'Что-то пошло не так')
+            return HttpResponseRedirect(reverse('authapp:register'))
 
 
 class LogoutView(TemplateView):
